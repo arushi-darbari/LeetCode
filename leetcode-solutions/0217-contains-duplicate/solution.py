dc7c50c34e5fc66a2f4d26a5c03bc31019d26410
arushi-darbari <darbariarushi14@gmail.com>
2026-06-18 06:55:32 +0000

    Sync LeetCode submission Runtime - 17 ms (48.28%), Memory - 31.4 MB (92.18%)

--- leetcode-solutions/0217-contains-duplicate/solution.py
+++ leetcode-solutions/0217-contains-duplicate/solution.py
@@ -1,11 +1,4 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        dic=set()
-        for num in nums:
-            if num in dic:
-                return True
-            dic.add(num)
-
-        return False
-
+        return len(nums)!=len(set(nums))
         
