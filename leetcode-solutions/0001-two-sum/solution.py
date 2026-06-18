0e09ade014eec7824377156e4db3b06e3b97dd11
arushi-darbari <darbariarushi14@gmail.com>
2026-06-18 06:54:11 +0000

    Sync LeetCode submission Runtime - 1 ms (61.30%), Memory - 20.3 MB (58.47%)

--- leetcode-solutions/0001-two-sum/solution.py
+++ leetcode-solutions/0001-two-sum/solution.py
@@ -1,10 +1,17 @@
+from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result=[]
-        arr={}
-        for index, value in enumerate(nums):
-            temp=target-value
-            if temp in arr:
-                return (index,arr[temp])
+        # for i in range(len(nums)):
+        #     for j in range(1,len(nums)):
+        #         if nums[i]+nums[j]==target and i!=j:
+        #             return [i,j]
+        
+        arr=defaultdict(int)
+
+        for index,value in enumerate(nums):
+            ele=target-value
+            if ele in arr:
+                return [index,arr[ele]]
+            
             else:
-                arr[value]=index
+                arr[value]=(index)
