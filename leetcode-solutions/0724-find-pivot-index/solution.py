59a3e152fcc942bb3878496db4da8377a42b22df
arushi-darbari <darbariarushi14@gmail.com>
2026-08-26 16:31:05 +0000

    Sync LeetCode submission Runtime - 1 ms (93.73%), Memory - 20.2 MB (81.86%)

---
+++ leetcode-solutions/0724-find-pivot-index/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum=sum(nums)
+        left_sum=0
+        for i, value in enumerate(nums):
+            if left_sum==total_sum-left_sum-value:
+                return i
+            
+            left_sum+=value
+
+        return -1
